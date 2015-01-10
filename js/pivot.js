19b881a934acbdb4b40fb6b2f73ed698559c608c
Jon Lewis <jon.lewis1@gmail.com>
2015-01-10 14:28:12 -0500

    Bug fixes

--- js/pivot.js
+++ js/pivot.js
@@ -1,28 +1,59 @@
-$(".description .menu li").click(
+var hash = document.URL.substr(document.URL.indexOf('#')+1); 
+var menuItem = $(".description .menu li");
+var contentArea = $(".content-area");
+
+//Homepage deep linking
+if(hash =="mgmt-content"){
+	contentArea.removeClass("active");
+	menuItem.removeClass("active");
+	$("#mgmt-btn").addClass("active");
+	$("#mgmt-content").addClass("active");
+}
+else if (hash == "cleanse-content"){
+	contentArea.removeClass("active");
+	menuItem.removeClass("active");
+	$("#cleanse-btn").addClass("active");
+	$("#cleanse-content").addClass("active");
+}
+else if (hash == "market-content"){
+	contentArea.removeClass("active");
+	menuItem.removeClass("active");
+	$("#market-btn").addClass("active");
+	$("#market-content").addClass("active");
+}
+
+//Menu functions
+menuItem.click(
 	function(){
 		if(!$(this).hasClass("active"))
 		{
-			$(".description .menu li").removeClass("active");
+			menuItem.removeClass("active");
 			$(this).addClass("active");
 
-			if($(this).is("#cleanse-btn")) 
+			if($(this).is(":nth-child(1)")) 
 			{
-				$(".content-area").hide();
-				$("#cleanse-content").show();
+				contentArea.removeClass("active");
+				$(".content-area:eq(0)").addClass("active");
 			} 
-			else if($(this).is("#mgmt-btn")) 
+			else if($(this).is(":nth-child(2)")) 
 			{
-				$(".content-area").hide();
-				$("#mgmt-content").show();
+				contentArea.removeClass("active");
+				$(".content-area:eq(1)").addClass("active");
 			} 
-			else if($(this).is("#market-btn")) 
+			else if($(this).is(":nth-child(3)")) 
 			{
-				$(".content-area").hide();
-				$("#market-content").show();
+				contentArea.removeClass("active");
+				$(".content-area:eq(2)").addClass("active");
+			}
+			else if($(this).is(":nth-child(4)")) 
+			{
+				contentArea.removeClass("active");
+				$(".content-area:eq(3)").addClass("active");
 			}
 		} 
 	});
 
+//Mobile toggle buttons
 $("#previous span").click(
 	function(){
 		$(".content-area.active").prev().addClass("active");
@@ -37,6 +68,11 @@ $("#next span").click(
 		$(".content-area.active").prev().removeClass("active");
 });
 
+//Main Nav
 $("#nav").click(function(){
 	$("#nav ul").toggle();
+
+});
+$("#nav #navigation-menu ul li").hover(function(){
+	$(this).toggleClass("active");
 });
